8cf26bbfdfead1d03ed078668447953a3cc327b5
sagihe <sagihe@mail.tau.ac.il>
2024-07-26 01:53:19 +0300

    Finished guest and host registering

--- src/EvacU/app.py
+++ src/EvacU/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from main import *
 import json
 
 app = Flask(__name__)
@@ -12,10 +13,18 @@ def hello_world():
 
 @app.route('/GuestData', methods=['POST'])
 def guest_data():
-    data = request.json
+    guest_data = request.json
+    available_hosts = new_guest(guest_data)
+    print(available_hosts)
+    return jsonify(available_hosts), 200
 
-    print(data)
-    return {"blulpup":"bhgh"}, 200
+
+@app.route('/HostData', methods=['POST'])
+def host_data():
+    host_data = request.json
+    new_host(host_data)
+    return {"Adding Host: " : "success" }, 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/EvacU/main.py
+++ src/EvacU/main.py
@@ -5,14 +5,16 @@ import sqlite3
 DB_NAME: str = "UsersDB.db"
 
 
-def new_guest():
-    # add to database
-    # scan for hosts
-    pass
+def new_guest(data):
+    guest = Guest(data["id"], data["first_name"], data["last_name"], data["city"], data["number_of_guests"], data["info"])
+    add_guest_to_db(guest)
+    available_hosts = scan_for_hosts(guest)
+    return available_hosts
 
-
-def new_host():
-    pass
+def new_host(data):
+    host = Host(data["id"], data["first_name"], data["last_name"], data["city"], data["max_guests"],
+                  data["requirements"])
+    add_host_to_db(host)
 
 
 def add_guest_to_db(guest):
@@ -27,13 +29,31 @@ def add_guest_to_db(guest):
             print("Database is locked")
 
 
+def scan_for_hosts(guest):
+    hosts_list = []
+    try:
+        with sqlite3.connect(DB_NAME) as conn:
+            command = """ 
+            SELECT * FROM Hosts 
+            WHERE Availability = 1 AND ? <= Max_Guests;"""
+            cursor = conn.execute(command, (guest.number_of_guests, ))
+            rows = cursor.fetchall()
+            columns = [column[0] for column in cursor.description]
+            for row in rows:
+                hosts_list.append(dict(zip(columns, row)))
+            conn.commit()
+    except sqlite3.OperationalError as err:
+        if "database is locked" in str(err):
+            print("Database is locked")
+    return hosts_list
+
+
 def add_host_to_db(host):
     try:
         with sqlite3.connect(DB_NAME) as conn:
             command = """INSERT OR IGNORE INTO Hosts (ID, First_Name, Last_Name, City, max_guests, Availability, requirements) 
             VALUES(?, ?, ?, ?, ?, 1, ?);"""
-            conn.execute(command,
-                         (host.id, host.first_name, host.last_name, host.city, host.max_guests, host.requirements))
+            conn.execute(command, (host.id, host.first_name, host.last_name, host.city, host.max_guests, host.requirements))
             conn.commit()
     except sqlite3.OperationalError as err:
         if "database is locked" in str(err):
@@ -41,10 +61,13 @@ def add_host_to_db(host):
 
 
 def main():
-    guest = Guest("207190401", "hagi", "Eis", "Shoham", 3, "Has huge bulbul")
+    guest = Guest("307190406", "hagi", "Eis", "Shoham", 2, "Has huge bulbul")
     add_guest_to_db(guest)
-    host = Host("207190406", "Sagit", "Eis", "ashdod", 3, "Has microscopic bulbul")
-    add_host_to_db(host)
+    host1 = Host("207490406", "Ido", "Eis", "Ashdod", 3, "Has microscopic bulbul")
+    host2 = Host("807490406", "Yoav", "Eis", "Ashdod", 3, "Has microscopic bulbul")
+    add_host_to_db(host1)
+    add_host_to_db(host2)
+    print(f"Hosts: {scan_for_hosts(guest)}")
 
 
 if __name__ == "__main__":
