19e12f77f583a53db3b0df582fa66d240973338b
Ido-Zacharia <idozacharia@mail.tau.ac.il>
2024-07-25 23:39:16 +0300

    ashdod

--- src/EvacU/main.py
+++ src/EvacU/main.py
@@ -6,8 +6,8 @@ DB_NAME: str = "UsersDB.db"
 
 
 def new_guest():
-    #add to data base
-    #scan for hosts
+    # add to database
+    # scan for hosts
     pass
 
 
@@ -28,12 +28,23 @@ def add_guest_to_db(guest):
 
 
 def add_host_to_db(host):
-    pass
+    try:
+        with sqlite3.connect(DB_NAME) as conn:
+            command = """INSERT OR IGNORE INTO Hosts (ID, First_Name, Last_Name, City, max_guests, Availability, requirements) 
+            VALUES(?, ?, ?, ?, ?, 1, ?);"""
+            conn.execute(command,
+                         (host.id, host.first_name, host.last_name, host.city, host.max_guests, host.requirements))
+            conn.commit()
+    except sqlite3.OperationalError as err:
+        if "database is locked" in str(err):
+            print("Database is locked")
 
 
 def main():
-    guest = Guest("207190406", "Sagi", "Eis", "Shoham", 3, "Has huge bulbul")
+    guest = Guest("207190401", "hagi", "Eis", "Shoham", 3, "Has huge bulbul")
     add_guest_to_db(guest)
+    host = Host("207190406", "Sagit", "Eis", "ashdod", 3, "Has microscopic bulbul")
+    add_host_to_db(host)
 
 
 if __name__ == "__main__":
